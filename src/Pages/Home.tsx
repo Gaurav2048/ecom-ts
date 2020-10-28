9d9083127f3dc97777db447a736bd07a54e0560d
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-28 17:37:46 +0530

    Consume redux state from pages

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootStore } from '../State/Store';
+import { getProducts } from '../State/Action/ProductActions';
 
 const Home = (props: RouteComponentProps) => {
+  const dispatch = useDispatch();
+  const products = useSelector((state: RootStore) => state.products);
+  console.log(products.products);
+
+  useEffect(() => {
+    dispatch(getProducts(10));
+  }, []);
+
   return <div>Home</div>;
 };
 
