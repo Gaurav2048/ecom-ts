3f987b655895e9e2a093840e100f74628e78ad53
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-28 21:25:55 +0530

    Implement card component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,17 +3,43 @@ import { RouteComponentProps } from '@reach/router';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootStore } from '../State/Store';
 import { getProducts } from '../State/Action/ProductActions';
+import { Card } from '../Components/ItemCards/Card';
+import { product } from '../State/Action/ProductActionTypes';
+import styled from 'styled-components';
+
+const Container = styled.section`
+  padding: 0.5em;
+  columns: 3;
+  column-gap: 15px;
+  background-color: #f7f7f7;
+  display: 'inline-block';
+`;
 
 const Home = (props: RouteComponentProps) => {
   const dispatch = useDispatch();
-  const products = useSelector((state: RootStore) => state.products);
-  console.log(products.products);
+  const productsState = useSelector((state: RootStore) => state.productsInfo);
+  console.log(productsState);
 
   useEffect(() => {
     dispatch(getProducts(10));
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <Container>
+      {productsState.products &&
+        productsState.products.map((product: product) => (
+          <Card key={product.id}>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Image>{product.image}</Card.Image>
+            <Card.Description>{product.description}</Card.Description>
+          </Card>
+        ))}
+      {productsState.products &&
+        productsState.products.map((product: product) => {
+          console.log(product.image);
+        })}
+    </Container>
+  );
 };
 
 export default Home;
