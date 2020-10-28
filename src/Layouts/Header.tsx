c83573db15ee6790939ba55f1ba78cf3cbde3b4c
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-28 21:26:55 +0530

    Add header

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: left;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 0.5em;
+  background: papayawhip;
+`;
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <Wrapper>
+      <Title> Type Commerce </Title>
+    </Wrapper>
+  );
 };
 
 export default Header;
