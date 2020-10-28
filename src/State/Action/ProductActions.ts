9632cf2fae3e77ba595e831b216cbaa106c64c51
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-28 17:36:50 +0530

    Add store, reducer and actions to the app

---
+++ src/State/Action/ProductActions.ts
@@ -0,0 +1,30 @@
+import {
+  LOADING_PRODUCTS,
+  LOADING_PRODUCTS_SUCCESS,
+  LOADING_PRODUCTS_FAILED,
+  productsDispatchType,
+} from './ProductActionTypes';
+import { Dispatch } from 'redux';
+import axios from 'axios';
+
+export const getProducts = (limit: number) => async (
+  dispatch: Dispatch<productsDispatchType>
+) => {
+  dispatch({
+    type: LOADING_PRODUCTS,
+  });
+
+  try {
+    const res = await axios.get(
+      `https://fakestoreapi.com/products?limit=${limit}`
+    );
+    dispatch({
+      type: LOADING_PRODUCTS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: LOADING_PRODUCTS_FAILED,
+    });
+  }
+};
